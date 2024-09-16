39e1400a9956eb2968fc05942884254b8bcbed75
KimLeduc <kim.leduc13@hotmail.com>
2024-09-16 19:56:52 -0400

    Switch

--- src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
+++ src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
@@ -6,31 +6,43 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import ca.ulaval.glo4002.mockexercise.do_not_edit.CartFactory;
+import ca.ulaval.glo4002.mockexercise.do_not_edit.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 
+@ExtendWith(MockitoExtension.class)
 public class StartByTestingThisTest {
 
     private StartByTestingThis service;
     @Mock
     private CartFactory cartFactory;
 
+    @Mock
+    private ProductRepository productRepository;
+
     private final String EMAIL = "test@beaubrun.com";
+
+    private final String SKU = "12345";
 
     @BeforeEach
     public void setUp() {
-        service = new StartByTestingThis(cartFactory);
+        service = new StartByTestingThis(cartFactory, productRepository);
     }
 
     @Test
     public void givenAnEmail_whenOneClickBuy_ThenCartIsCreated() {
-        Cart cart = new Cart(EMAIL);
-        when(cartFactory.create(EMAIL)).thenReturn(cart);
-
         service.oneClickBuy(EMAIL, null);
 
-        verify(cartFactory, times(1));
+        verify(cartFactory, times(1)).create(EMAIL);
+    }
+    @Test
+    public void givenASKU_whenOneClickBuy_ThenProductIsFound() {
+        service.oneClickBuy(EMAIL, SKU);
+
+        verify(productRepository, times(1)).findBySku(SKU);
     }
 }
